ea00cb1f985023de7bc34cd811e3dee4368b44fc
Hernán Cervera <hjcerveram@hotmail.com>
2021-09-18 09:51:18 -0500

    Refactor C++ solution to avoid duplicate code

--- debugging/palindromes/Solution.cpp
+++ debugging/palindromes/Solution.cpp
@@ -5,24 +5,39 @@
 
 using namespace std;
 
+string toLowercase(string phrase)
+{
+  string output = phrase;
+
+  // From: https://stackoverflow.com/a/313990/12591546
+  transform(output.begin(), output.end(), output.begin(),
+            [](unsigned char c)
+            { return tolower(c); });
+
+  return output;
+}
+
+string removeWhitespace(string phrase)
+{
+  string output = phrase;
+
+  // From: https://stackoverflow.com/a/83538/12591546
+  output.erase(remove_if(output.begin(), output.end(), ::isspace), output.end());
+
+  return output;
+}
+
+string reverseString(string phrase)
+{
+  string output = phrase;
+  reverse(output.begin(), output.end());
+  return output;
+}
+
 string isPalindrome(string phrase)
 {
-  string originalLowercaseNoSpaces = phrase;
-  string reversedOriginalLowercaseNoSpaces = phrase;
-
-  transform(originalLowercaseNoSpaces.begin(), originalLowercaseNoSpaces.end(), originalLowercaseNoSpaces.begin(),
-            [](unsigned char c)
-            { return tolower(c); });
-  originalLowercaseNoSpaces.erase(remove_if(originalLowercaseNoSpaces.begin(), originalLowercaseNoSpaces.end(), ::isspace), originalLowercaseNoSpaces.end());
-
-  transform(reversedOriginalLowercaseNoSpaces.begin(), reversedOriginalLowercaseNoSpaces.end(), reversedOriginalLowercaseNoSpaces.begin(),
-            [](unsigned char c)
-            { return tolower(c); });
-  reversedOriginalLowercaseNoSpaces.erase(remove_if(reversedOriginalLowercaseNoSpaces.begin(), reversedOriginalLowercaseNoSpaces.end(), ::isspace), reversedOriginalLowercaseNoSpaces.end());
-  reverse(reversedOriginalLowercaseNoSpaces.begin(), reversedOriginalLowercaseNoSpaces.end());
-
-  cout << originalLowercaseNoSpaces << "\n";
-  cout << reversedOriginalLowercaseNoSpaces << "\n";
+  string originalLowercaseNoSpaces = removeWhitespace(toLowercase(phrase));
+  string reversedOriginalLowercaseNoSpaces = reverseString(removeWhitespace(toLowercase(phrase)));
 
   return originalLowercaseNoSpaces.compare(reversedOriginalLowercaseNoSpaces) == 0 ? "yes" : "no";
 }
